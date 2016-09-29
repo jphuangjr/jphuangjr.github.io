136db9487a2dafd11b944589c61f13bdccb8baae
Joshua Paul Jr. Huang <jphuangjr@gmail.com>
2016-09-29 13:47:19 -0700

    Add files via upload

---
+++ compliance.js
@@ -0,0 +1,116 @@
+
+(function(){
+	return {
+		topics : [
+			{
+				title: "CIS Benchamark for Oracle Database",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "Health Insurance Portability and Account...",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "I - Mission Critical Classified",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "SANS/CIS Top 20 Critical Security Control",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "CIS Benchamark for Oracle Database",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "Health Insurance Portability and Account...",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "I - Mission Critical Classified",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "SANS/CIS Top 20 Critical Security Control",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "CIS Benchamark for Oracle Database",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "Health Insurance Portability and Account...",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "I - Mission Critical Classified",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			},{
+				title: "SANS/CIS Top 20 Critical Security Control",
+				lastpost: "1305857807",
+				breakDown: {
+					failing: 225,
+					passing: 125,
+					error: 150
+				},
+				username:"test"
+			}
+		]
+	}
+})()
